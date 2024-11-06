996a76b3c7eb59d541a2d37532cc3e20c1addb05
LilElmo09 <jim.sntd@gmail.com>
2024-11-06 04:12:07 -0300

    Añadir pruebas de inicio de sesión y datos de usuario, eliminar pruebas y archivos obsoletos

---
+++ cypress/component/home.cy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { mount } from 'cypress/react'
+import Home from '/FrontEnd-ENE/client/src/components/Home/Home'
+
+describe('Home Component', () => {
+    it('renders correctly', () => {
+        mount(<Home />)
+        cy.get('selector').should('contain', 'expected text')
+    })
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,45 @@
+describe('Login Test', () => {
+    const url = 'http://localhost:3000/ingresar'
+    let email
+    let contrasena
+
+    before(() => {
+        cy.fixture('data.json').then((data) => {
+            email = data.email
+            contrasena = data.contrasena
+        })
+    })
+
+    it('should successfully log in with valid credentials', () => {
+        cy.visit(url)
+        //cy.title().should('eq', 'Ingresar')
+        cy.get('#email').should('be.visible').type(email)
+        cy.get('#contraseña').should('be.visible').type(contrasena)
+        cy.get('.btn').click()
+        //cy.url().should('not.eq', url)
+    })
+
+    it('should detect if email is missing', () => {
+        cy.visit(url)
+        //cy.title().should('eq', 'Ingresar')
+        cy.get('#contraseña').should('be.visible').type(contrasena)
+        cy.get('#email').should('be.visible')
+        cy.get('.btn').click()
+        cy.wait(500)
+        cy.get('.invalid-feedback')
+            .should('be.visible')
+            .and('contain', 'El corro es obligatorio')
+    })
+
+    it('should detect if password is missing', () => {
+        cy.visit(url)
+        //cy.title().should('eq', 'Ingresar')
+        cy.get('#email').should('be.visible').type(email)
+        cy.get('#contraseña').should('be.visible')
+        cy.get('.btn').click()
+        cy.wait(500)
+        cy.get('.invalid-feedback')
+            .should('be.visible')
+            .and('contain', 'La contraseña es obligatoria')
+    })
+})
