15881a4ec6f424d497627d8c9805a838961d3108
LilElmo09 <jim.sntd@gmail.com>
2024-11-06 02:59:29 -0300

    avance 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-        theme: 'dark',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
-})
+    theme: "dark",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
