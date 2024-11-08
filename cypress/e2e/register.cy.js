e26178c6e0b847165d8cd4f9d04b7b7b596fb1eb
LilElmo09 <jim.sntd@gmail.com>
2024-11-08 18:50:39 -0300

    Eliminar verificación del título en las pruebas de registro y agregar nuevas comprobaciones para la creación de usuario

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -15,9 +15,10 @@ describe('Login Test', () => {
             emailmalo = data.emailmalo
         })
     })
+
     it('Check that the error appears for empty fields', () => {
         cy.visit(url)
-        cy.title().should('eq', 'Registrar')
+        //cy.title().should('eq', 'Registrar')
         cy.get('.btn').click()
         cy.get(':nth-child(1) > .invalid-feedback')
             .should('be.visible')
@@ -32,24 +33,44 @@ describe('Login Test', () => {
             .should('be.visible')
             .and('contain', 'La contraseña es obligatoria')
     })
+
     it('Check that the error appears for the bad email', () => {
         cy.visit(url)
-        cy.title().should('eq', 'Registrar')
+        //cy.title().should('eq', 'Registrar')
         cy.get(':nth-child(3) > .form-control').type(emailmalo)
         cy.get('.btn').click()
         cy.get(':nth-child(3) > .invalid-feedback')
             .should('be.visible')
             .and('contain', 'El formato de email no es válido')
     })
+
     it('Check that the user is created correctly', () => {
         cy.visit(url)
-        cy.title().should('eq', 'Registrar')
+        //cy.title().should('eq', 'Registrar')
         cy.get(':nth-child(1) > .form-control').type(nombre)
         cy.get(':nth-child(2) > .form-control').type(apellido)
         cy.get(':nth-child(3) > .form-control').type(email)
         cy.get(':nth-child(4) > .form-control').type(contrasena)
         cy.get('.btn').click()
         //Hacer la comprobacion en futura version
-        //cy.url().should('eq', 'http://localhost:3000/login')
+        cy.get('.swal2-popup')
+            .should('be.visible')
+            .and('contain', 'Usuario registrado correctamente.')
+        cy.url().should('eq', 'http://localhost:3000/login')
+    })
+
+    it('Check that the user already exists', () => {
+        cy.visit(url)
+        //cy.title().should('eq', 'Registrar')
+        cy.get(':nth-child(1) > .form-control').type(nombre)
+        cy.get(':nth-child(2) > .form-control').type(apellido)
+        cy.get(':nth-child(3) > .form-control').type(email)
+        cy.get(':nth-child(4) > .form-control').type(contrasena)
+        cy.get('.btn').click()
+        //Hacer la comprobacion en futura version
+        cy.get('.swal2-popup')
+            .should('be.visible')
+            .and('contain', 'Error al registrar el usuario.')
+        cy.url().should('eq', url)
     })
 })
