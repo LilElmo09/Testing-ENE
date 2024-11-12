b6893a939662e1ecaf35f112fa4313701974cae3
LilElmo09 <jim.sntd@gmail.com>
2024-11-12 20:09:27 -0300

    combio de sistema

--- cypress/e2e/backend/addRole.cy.js
+++ cypress/e2e/backend/addRole.cy.js
@@ -1,9 +1,14 @@
 describe('addRole', () => {
     it('should add a role to a user', () => {
-        cy.request('POST', 'http://localhost:3000/admin/add-role/:usuario_id', {
-            id: 123,
-            rol_id: 1,
-        }).then((response) => {
+        const usuario_id = 3
+        cy.request(
+            'POST',
+            `http://localhost:3000/admin/add-role/${usuario_id}`,
+            {
+                id: 3,
+                rol_id: 1,
+            }
+        ).then((response) => {
             expect(response.status).to.eq(200)
         })
     })
